6e571d285fb34488c3b06382aa2a925a7b7aac25
kalmia <kalmiakimia@yahoo.com.tw>
2024-05-28 11:36:41 +0800

    0528

--- window/index.py
+++ window/index.py
@@ -1,8 +1,20 @@
 #自動開檔案
-with open('names.txt',encoding='utf-8') as file:
-    content:str = file.read()  #回傳字串
-#print(content)   #加print(content)可以列印出換行的名子
-names:list[str]=content.split() #算出有幾個名字 
-len(names)                       #算出有幾個名字  
-for name in names:
-    print(name)
+# with open('names.txt',encoding='utf-8') as file:
+#     content:str = file.read()  #回傳字串
+# #print(content)   #加print(content)可以列印出換行的名子
+# names:list[str]=content.split() #算出有幾個名字 
+# len(names)                       #算出有幾個名字  
+# for name in names:
+#     print(name)
+
+
+
+def get_names() -> list[str]:
+    with open('names.txt',encoding='utf-8') as file:
+        content:str = file.read()      #回傳字串
+    names:list[str] = content.split()  #算出有幾個名字 
+    return names
+# names:list[str]=get_names()   #文件變數 所以和上面不會起充突
+if __name__ == '__main__':
+    names:list[str] = get_names()
+    print(names)    
