34cf47a78aed730b137afe799bca1a77aeb99f31
kalmiavicky <kalmiakimia@ythoo.com.tw>
2024-07-09 09:06:17 +0800

    20240709-0906

--- insert_youbike_data/cron copy 2.py
+++ insert_youbike_data/cron copy 2.py
@@ -2,7 +2,8 @@ from crontab import CronTab
 import os
 cron = CronTab(user=True)
 path = os.path.abspath("./lesson2.py")
-job = cron.new(command=f"/opt/miniconda3/envs/venv1/bin/python '{path}'")
-job.minute.every(10)
+job = cron.new(command=f"/home/pi/Documents/vicky_window/insert_youbike_data '{path}'")
+
+job.minute.every(2)
 job.set_comment("Output hello world")
 cron.write()
