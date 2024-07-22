bb3e234f8a9858c442ff19fd99a7fbf564ec140c
kalmia <kalmiakimia@yahoo.com.tw>
2024-07-22 15:50:36 +0800

    20240722-0350

---
+++ 20240722/callback5.py
@@ -0,0 +1,16 @@
+from dash import Dash, dcc, html, Input, Output, callback
+import data
+
+app=Dash(__name__)
+
+areas = [tup[0] for tup in data.get_areas()]
+app.layout = html.Div([
+   dcc.Dropdown(
+        options = areas,
+        value= areas[0],           
+        id='areas'
+   )
+
+])
+if __name__ == '__main__':
+   app.run(debug=True)
